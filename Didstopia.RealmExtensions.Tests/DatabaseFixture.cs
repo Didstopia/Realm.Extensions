a84c5b043371b69ed647fccf9294f75a4b4fcc54
Pauli Jokela <pauli.jokela@didstopia.com>
2017-07-07 13:37:55 +0300

    Refactoring

---
+++ Didstopia.RealmExtensions.Tests/DatabaseFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Realms;
+using System.Linq;
+
+namespace Didstopia.RealmExtensions.Tests
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public Realm Realm => Realm.GetInstance();
+
+        public DatabaseFixture()
+        {
+            // Create and prepare database
+            //Realm = Realm.GetInstance();
+        }
+
+        public void Dispose()
+        {
+            // TODO: Rewrite at some point
+            try
+            {
+                // Clean and dispose of database
+                Realm.Write(() =>
+                {
+                    Realm.RemoveAll();
+                });
+                Realm.Dispose();
+            } catch { }
+        }
+
+        public void Add(RealmObject realmObject, bool update = false)
+        {
+            Realm.Write(() =>
+            {
+                Realm.Add(realmObject, update);
+            });
+        }
+
+        public void Remove(RealmObject realmObject)
+        {
+            Realm.Write(() =>
+            {
+                Realm.Remove(realmObject);
+            });
+        }
+    }
+}
---
+++ Didstopia.RealmExtensions.Tests/UnitTests.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using Didstopia.RealmExtensions;
+
+namespace Didstopia.RealmExtensions.Tests
+{
+    [Collection("Realm collection")]
+    public class UnitTests
+    {
+        DatabaseFixture _databaseFixture { get; set; }
+
+        public UnitTests(DatabaseFixture fixture)
+        {
+            _databaseFixture = fixture;
+        }
+
+        // TODO: Can we make this so that only the CI will skip this, but our IDE won't?
+        [Fact(Skip = "Skipping test because it's still a work in progress and broken")]
+        public void TestRealmList()
+        {
+            var list = new RealmList<string>();
+            Assert.NotNull(list);
+            Assert.True(list.Count == 0);
+
+            list.Add("Test #1");
+            Assert.True(list.Count == 1);
+
+            try
+            {
+                _databaseFixture.Add(list);
+            }
+            catch (Exception e)
+            {
+                Assert.Null(e);
+            }
+
+            try
+            {
+                _databaseFixture.Remove(list);
+            }
+            catch (Exception e)
+            {
+                Assert.Null(e);
+            }
+        }
+    }
+}
